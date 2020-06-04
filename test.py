5dafbe1d529d54b27929886124429d2bb7aa0ab3
ttran90 <ttran90@gatech.edu>
2020-06-03 23:16:59 -0400

    update test

--- test.py
+++ test.py
@@ -4,39 +4,31 @@ import solver
 class TestSudokuSolver(unittest.TestCase):
     def setUp(self):
         g0 = [1,2,3,4,5,6,7,8,9]
-        g1 = [4,5,6,7,8,9,1,2,3]
-        g2 = [7,8,9,0,0,0,0,0,0]
-        g3 = [2,3,4,0,0,0,0,0,0]
-        g4 = [5,6,7,0,0,0,0,0,0]
-        g5 = [8,9,1,0,0,0,0,0,0]
-        g6 = [3,4,5,0,0,0,0,0,0]
-        g7 = [6,7,8,0,0,0,0,0,0]
-        g8 = [9,1,2,0,0,0,0,0,0]
+        g1 = [4,5,0,7,0,9,1,0,3]
+        g2 = [7,8,9,1,2,3,4,5,6]
+        g3 = [2,3,4,5,6,7,8,9,1]
+        g4 = [5,6,7,8,9,1,2,3,4]
+        g5 = [8,9,1,2,3,4,5,6,7]
+        g6 = [0,4,5,6,0,8,9,1,2]
+        g7 = [6,0,8,9,1,2,3,4,5]
+        g8 = [9,0,2,3,0,5,6,7,8]
         g = [g0, g1, g2, g3, g4, g5, g6, g7, g8]
         self.game = solver.Board(g)
+        self.possible_options = {1,2,3,4,5,6,7,8,9}
 
-    def test_done_across(self):
-        self.assertTrue(self.game.done_across(0))
-        self.assertTrue(self.game.done_across(1))
-        self.assertFalse(self.game.done_across(2))
-        self.assertFalse(self.game.done_across(3))
-
-
-    def test_done_down(self):
-        self.assertTrue(self.game.done_down(0))
-        self.assertTrue(self.game.done_down(1))
-        self.assertFalse(self.game.done_down(3))
+    def test_check_across(self):
+        self.assertEqual(self.game.check_across(self.possible_options, 1), {6,2,8})
     
-    def test_done_square(self):
-        self.assertTrue(self.game.done_square(0,0))
-        self.assertTrue(self.game.done_square(0,2))
-        self.assertTrue(self.game.done_square(1,1))
-        self.assertTrue(self.game.done_square(1,0))
-        self.assertTrue(self.game.done_square(2,2))
-        self.assertFalse(self.game.done_square(3,4))
-        self.assertFalse(self.game.done_square(5,7))
-        self.assertFalse(self.game.done_square(6,3))
-        
+    def test_check_down(self):
+        self.assertEqual(self.game.check_down(self.possible_options, 4), {8,7,4})
+
+    def test_check_square(self):
+        self.assertEqual(self.game.check_square(self.possible_options, 6,2), {3,7,1})
+
+    def test_get_options(self):
+        self.assertEqual(self.game.get_options(6, 0), {3})
+
+         
 
 if __name__ == '__main__':
     unittest.main()
