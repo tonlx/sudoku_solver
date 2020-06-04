8be27328758bc6898630e4387893c499e00497a4
ttran90 <ttran90@gatech.edu>
2020-06-03 23:59:04 -0400

    easy solver

--- solver.py
+++ solver.py
@@ -1,4 +1,5 @@
 import math
+import time
 
 class Board:
     
@@ -35,7 +36,26 @@ class Board:
         possible_options = self.check_square(possible_options, row, col)
         return possible_options
 
-        
+    def solve(self):
+        run_count = 0
+        done = False
+        while not done:
+            count = 0
+            for row in range(Board.size):
+                for col in range(Board.size):
+                    if not self.grid[row][col] == 0:
+                        count += 1
+                    else:
+                        possible_options = self.get_options(row, col)
+                        if len(possible_options) == 1:
+                            self.grid[row][col] = possible_options.pop()
+                            count += 1
+                    done = count == self.size**2
+            run_count += 1
+            print('Run {}...{}'.format(run_count, count))
+            time.sleep(0.5)
+        self.show()
+
     def show(self):
         for row in range (9):
             if row % 3 == 0:
@@ -48,3 +68,19 @@ class Board:
                     print(' ', end = '')
             print('|')
         print('-------------------')
+
+
+
+g0 = [0, 0, 8, 0, 0, 0, 7, 0, 0] 
+g1 = [3, 0, 0, 0, 2, 5, 0, 0, 0]
+g2 = [9, 0, 2, 0, 0, 0, 0, 3, 5]
+g3 = [0, 0, 0, 2, 0, 9, 0, 0, 8]
+g4 = [0, 2, 0, 6, 0, 8, 0, 9, 0]
+g5 = [4, 0, 0, 5, 0, 3, 0, 0, 0]
+g6 = [8, 4, 0, 0, 0, 0, 2, 0, 3]
+g7 = [0, 0, 0, 7, 8, 0, 0, 0, 1]
+g8 = [0, 0, 5, 0, 0, 0, 8, 0, 0]
+
+g = [g0, g1, g2, g3, g4, g5, g6, g7, g8]
+game = Board(g)
+game.solve()
